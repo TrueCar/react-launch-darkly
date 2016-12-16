7437d10259d5c7936f1890e3ba6ffc7c86f2195d
Jacob Moretti <jmoretti@truecar.com>
2016-12-15 21:08:53 -0800

    Adds getAllFeatureFlags function and exposes it

--- src/lib/launchDarkly.js
+++ src/lib/launchDarkly.js
@@ -4,3 +4,7 @@ export function ldBrowserInit (key, user) {
   return launchDarklyBrowser.initialize(key, user);
 }
 
+export function getAllFeatureFlags (key, user) {
+  const ldClient = ldBrowserInit(key, user);
+  return ldClient.allFlags();
+}
