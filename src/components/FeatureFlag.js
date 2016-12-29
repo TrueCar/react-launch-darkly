bbc803556d09cbfeb9e215b1641a136830f3c289
Todd Williams <toddsurfs@icloud.com>
2016-12-28 16:46:01 -0800

    move to functional stateless components for FeatureFlag and LaunchDarkly
    Since the FeatureFlag and LaunchDarkly components are not using any
    local state there is a performance advantage of converting them to
    simple stateless functional components. It also makes for less code as
    well.

--- src/components/FeatureFlag.js
+++ src/components/FeatureFlag.js
@@ -1,26 +1,22 @@
 /* @flow */
-import React, { Component } from "react";
+import React from "react";
 import { Subscriber } from "react-broadcast";
 
 import { BROADCAST_CHANNEL } from "../constants/LaunchDarkly.js";
 import FeatureFlagRenderer from "./FeatureFlagRenderer";
 import { FeatureFlagType } from "../types/FeatureFlag";
 
-export default class FeatureFlag extends Component {
-  props: FeatureFlagType;
-
-  render () {
-    return (
-      <Subscriber channel={BROADCAST_CHANNEL}>
-        { (launchDarklyConfig) => (
-            <FeatureFlagRenderer
-              launchDarklyConfig={launchDarklyConfig}
-              {...this.props}
-            />
-          )
-        }
-      </Subscriber>
-    );
-  }
+export default function FeatureFlag (props:FeatureFlagType) {
+  return (
+    <Subscriber channel={BROADCAST_CHANNEL}>
+      { (launchDarklyConfig) => (
+          <FeatureFlagRenderer
+            launchDarklyConfig={launchDarklyConfig}
+            {...props}
+          />
+        )
+      }
+    </Subscriber>
+  );
 }
 
