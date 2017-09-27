ceee7fc8da70b6582148bf41f4b7ef6f5a1bffb7
Jacob Moretti <jacobmoretti@users.noreply.github.com>
2017-09-27 15:08:01 -0700

    Update react broadcast 0.3.1 (#42)
    * Upgrade react-broadcast and remove from devDependencies
    
    * Fix test that tried to render Subscriber without a parent Broadcast. Remove an old test

--- test/components/LaunchDarkly.test.js
+++ test/components/LaunchDarkly.test.js
@@ -1,9 +1,7 @@
-import React, { Component } from "react";
-import { shallow, mount } from "enzyme";
+import React from "react";
+import { shallow } from "enzyme";
 import { Broadcast } from "react-broadcast";
 import LaunchDarkly from "../../src/components/LaunchDarkly";
-import FeatureFlag from "../../src/components/FeatureFlag";
-import FeatureFlagRenderer from "../../src/components/FeatureFlagRenderer";
 import { BROADCAST_CHANNEL } from "../../src/constants/LaunchDarkly";
 
 describe("components/LaunchDarkly", () => {
@@ -61,77 +59,5 @@ describe("components/LaunchDarkly", () => {
     subject = shallow(<LaunchDarkly user={{name: "Kelly Slater"}}><div>Hi</div></LaunchDarkly>);
     child = subject.find("Broadcast");
     expect(child.props().value).toEqual(null);
-  });
-
-  // No longer a valid concern since we are not passing the clientId or user to any of the children.
-  // Instead the ldClient is initialized in the LaunchDarkly component and passed down.
-  // Keeping this test for now in case we need to alter it for some other concern.
-  it.skip("should propagate clientId and user even when shouldComponentUpdate is false somewhere above the flag renderer", () => {
-    class A extends Component {
-      props: {
-        children: any
-      };
-
-      shouldComponentUpdate () {
-        return false;
-      }
-
-      render () {
-        return (
-          <div>{this.props.children}</div>
-        );
-      }
-    }
-
-    class App extends Component {
-      constructor (props) {
-        super(props);
-        this.state = {
-          user: "i"
-        };
-
-        this._renderFeature = this._renderFeature.bind(this);
-        this._updateUser = this._updateUser.bind(this);
-      }
-
-      state: {
-        user: String
-      };
-
-      render () {
-        return (
-          <LaunchDarkly clientId="808080" user={this.state.user}>
-            <div>
-              <button id="updateUser" onClick={this._updateUser}>UpdateUser</button>
-              <A>
-                <FeatureFlag
-                  flagKey="abc"
-                  renderFeatureCallback={this._renderFeature}
-                />
-              </A>
-            </div>
-          </LaunchDarkly>
-        );
-      }
-
-      _renderFeature() {
-        return <div>feature render</div>;
-      }
-
-      _updateUser () {
-        this.setState({
-          user: "imarealuser"
-        });
-      }
-    }
-
-    const subject = mount(
-      <App />
-    );
-
-    const renderer = subject.find(FeatureFlagRenderer);
-    expect(renderer.prop("launchDarklyConfig").user).toEqual("i");
-    subject.find("#updateUser").simulate("click");
-    expect(renderer.prop("launchDarklyConfig").user).toEqual("imarealuser");
   });
 });
