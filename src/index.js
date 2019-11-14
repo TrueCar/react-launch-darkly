a663d7a421f381a71cc313a069d7d860f4aee1ca
olya777 <olya777@gmail.com>
2019-11-13 16:50:49 -0800

    export useFlags

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as LaunchDarkly } from "./components/LaunchDarkly";
 export { default as FeatureFlag } from "./components/FeatureFlag";
+export { default as useFlags } from "./hooks/useFlags";
 export { getAllFeatureFlags, identify, track, feature } from "./lib/utils";
